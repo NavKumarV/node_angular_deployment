ff64edb48defdd423586a3ff5412290bde559a39
Naveen Kumar V <40155185+NavKumarV@users.noreply.github.com>
2020-12-23 06:27:35 +0530

    creating setup_nodejs_angular.sh

---
+++ setup-nodejs-angular.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+echo "
+----------------------
+  NODE & NPM
+----------------------
+"
+
+# add nodejs 10 ppa (personal package archive) from nodesource
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+
+# install nodejs and npm
+sudo apt-get install -y nodejs
+
+
+echo "
+----------------------
+  FOREVER
+----------------------
+"
+
+# install pm2 with npm
+sudo npm install -g forever
+
+echo "
+----------------------
+  NGINX
+----------------------
+"
+
+# install nginx
+sudo apt-get install -y nginx
+
+
+echo "
+----------------------
+  UFW (FIREWALL)
+----------------------
+"
+
+# allow ssh connections through firewall
+sudo ufw allow OpenSSH
+
+# allow http & https through firewall
+sudo ufw allow 'Nginx Full'
+
+# enable firewall
+sudo ufw --force enable
